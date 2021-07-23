7cc586ce29d0f29c2540d77e2f25185ed9df1d6d
unknown <charafe.boulhout@univ-constantine2.dz>
2021-07-24 01:40:51 +0200

    adding files

---
+++ resources/views/front-end/index.blade.php
@@ -0,0 +1,25 @@
+<x-app-layout>
+  <div class="h-screen w-full pt-4 bg overflow-hidden">
+   <div class="custom-shape-divider-bottom-1615408481">
+      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+      </svg>
+    </div>
+    <div class="text-6xl text-white font-sans font-bold uppercase text-center">Majorinfo</div>
+    <div class="flex justify-between">
+    <div class="hidden lg:block relative -top-8 w-1/3 ">
+      <img draggable="false" src="assets/images/store.svg" alt="">
+    </div>
+    <div class="w-1/3 text-right font-sans text-white p-8">
+      <p class="text-4xl text-yellow-400 font-semibold">خدمات الإعلام الألى</p>
+      <ul class="text-2xl">
+        <li>تصليح أجهزة الحاسوب</li>
+        <li>فرمتة و اعادة تنصيب نظام التشغيل </li>
+        <li>صيانة و حل مختلف أعطال الحاسوب</li>
+      </ul>
+    </div>
+    
+    </div>
+    
+  </div>
+</x-app-layout>
